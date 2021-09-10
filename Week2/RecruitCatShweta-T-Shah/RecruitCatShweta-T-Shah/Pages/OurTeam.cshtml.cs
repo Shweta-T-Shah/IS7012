96b209268091308884cb1efbdee5d87c98a07a2f
ShwetaShah6100 <shahshweta61296@gmail.com>
2021-09-10 17:08:32 -0400

    Week2

---
+++ Week2/RecruitCatShweta-T-Shah/RecruitCatShweta-T-Shah/Pages/OurTeam.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitCatShweta_T_Shah.Pages
+{
+    public class OurTeamModel : PageModel
+    {
+        private readonly ILogger<OurTeamModel> _logger;
+
+        public OurTeamModel(ILogger<OurTeamModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}

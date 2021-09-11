a8d8f41a8426fa8f142d7c7ae8c7cfcee495dbd9
ShwetaShah6100 <shahshweta61296@gmail.com>
2021-09-11 00:43:22 -0400

    Final Week 2

---
+++ Week2/RecruitCatShweta-T-Shah/RecruitCatShweta-T-Shah/Pages/Apply.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+    namespace RecruitCatShweta_T_Shah.Pages
+{
+    public class ApplyModel : PageModel
+    {
+        private readonly ILogger<ApplyModel> _logger;
+
+        public ApplyModel(ILogger<ApplyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
